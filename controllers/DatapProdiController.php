827bda9dfcd3ae948b29bde443f4c53eb2838217
Zalfa-3 <fadhillahzalfa@gmail.com>
2023-05-08 15:33:08 +0800

    first commit

---
+++ controllers/DatapProdiController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace app\controllers;
+
+class DatapProdiController extends \yii\web\Controller
+{
+    public function actionDetailProdi()
+    {
+        return $this->render('detail-prodi');
+    }
+
+}
--- controllers/MahasiswaController.php
+++ controllers/MahasiswaController.php
@@ -4,9 +4,9 @@ namespace app\controllers;
 
 class MahasiswaController extends \yii\web\Controller
 {
-    public function actionProfil()
+    public function actionUbahKrs()
     {
-        return $this->render('profil');
+        return $this->render('ubah-krs');
     }
 
 }
